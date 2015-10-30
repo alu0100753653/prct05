b7961699965518fa1f62bd55ca3bf97d7a783ac6
alu0100753653 <alu0100753653@ull.edu.es>
2015-10-30 13:38:04 +0000

    Depuración del gcd.rb

---
+++ gcd.rb
@@ -0,0 +1,13 @@
+def gcd(n, d)
+    n1 = abs(n);
+    n2 = abs(d)
+    gcd = 1
+    k = 1
+    while k <= n1 and k <= n2
+        if n1 % k == 0 and n2 % k == 0
+            gcd = k
+        end
+        k += 1
+    end
+    return gcd
+end
